0e5a1ee72ba349feb1a37fd8ac37b2307be164d4
johnnie <johnnie@John>
2025-04-14 19:56:15 +0300

    done

--- Extensions/proxyswitcher/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("connectBtn").addEventListener("click", function() {
-    var proxy = document.getElementById("proxyList").value;
-    activateProxy(proxy);
-  });
-  
-  function activateProxy(proxy) {
-    chrome.runtime.sendMessage({ action: "activateProxy", proxy: proxy });
-  }
