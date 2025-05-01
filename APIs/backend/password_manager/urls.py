4f4e664627758d84070397eb6fe8f4ba52fe9b2f
ifconfigbrian <njugunabriian@gmail.com>
2025-05-01 17:05:21 +0300

    userlogin implementation

---
+++ APIs/backend/password_manager/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserRegistrationView, UserLoginView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('signup/', UserRegistrationView.as_view(), name='user-register'),
+    path('login/', UserLoginView.as_view(), name='user-login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
