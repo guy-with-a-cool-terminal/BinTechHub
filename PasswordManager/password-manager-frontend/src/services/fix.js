f66d05afa746afc9126ad742a4338d611d080974
ifconfigbrian <njugunabriian@gmail.com>
2025-05-16 16:05:57 +0300

    small fixes

--- PasswordManager/password-manager-frontend/src/services/fix.js
+++
@@ -1,9 +0,0 @@
-const getPasswords = async (page = 1) => { // add page parameter with default
-  try {
-    const response = await api.get(`/passwords/?page=${page}`);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-    throw new Error('Get Password List failed');
-  }
-};
