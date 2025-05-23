fd8e09f95108b8aa9525a49a2d8861729c05e7dc
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-05-23 15:43:54 +0300

    final trial

--- APIs/backend/backend/urls.py
+++ APIs/backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('password_manager.urls')),
     
     # MPESA integration URLs
