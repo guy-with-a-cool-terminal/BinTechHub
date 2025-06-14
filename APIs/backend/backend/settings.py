d955671076f957c0aa4c5f02fe8d1cd9da5c258a
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-06-14 18:10:12 +0300

    deployment

--- APIs/backend/backend/settings.py
+++ APIs/backend/backend/settings.py
@@ -131,6 +131,7 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://secure-vault-bintech.vercel.app",
+    "https://darajacredentialsmanager.vercel.app",
     "http://localhost:5173",  # for dev
 ]
 
